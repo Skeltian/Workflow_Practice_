a36529a77e56821776eac1ffd8ece80d1ece6763
Skeltian <33046504+Skeltian@users.noreply.github.com>
2021-12-27 15:43:23 -0600

    Create PacletInfo.m

---
+++ PacletInfo.m
@@ -0,0 +1,9 @@
+Paclet[
+	"Name" -> "TestPaclet",
+	"Version" -> "1.0.0",
+	"Creator" -> "Brandon Sutton <bsutton10294@gmail.com",
+	"Description" -> "Test paclet created for testing Github Action workflow",
+	"Extensions" -> {
+		{"Kernel", "Root"->".", "Context"->{"TestPaclet`"}}
+	}
+]
