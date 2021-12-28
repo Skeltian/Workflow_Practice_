67917e4d9317b80e60a262d8d4d6380d0291815b
Skeltian <33046504+Skeltian@users.noreply.github.com>
2021-12-28 10:37:25 -0600

    Update PacletInfo.m

--- DifferentPaclet/PacletInfo.m
+++ DifferentPaclet/PacletInfo.m
@@ -2,7 +2,7 @@
 
 Paclet[
 	"Name" -> "DifferentPaclet",
-	"Version" -> "1.0.1",
+	"Version" -> "1.0.2",
 	"Creator" -> "Brandon Sutton <bsutton10294@gmail.com",
 	"Description" -> "Test paclet created for testing Github Action workflow",
 	"Extensions" -> {
