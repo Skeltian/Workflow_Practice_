b934daa530c9ba85e3164f036215ea6891e533c9
Skeltian <33046504+Skeltian@users.noreply.github.com>
2021-12-27 19:19:07 -0600

    Delete PacletSite.m

--- TestPaclet/PacletSite.m
+++
@@ -1,1 +0,0 @@
-PacletSite[Paclet["Name" -> "TestPaclet", "Version" -> "1.0.0", "Creator" -> "Brandon Sutton <bsutton10294@gmail.com", "Description" -> "Test paclet created for testing Github Action workflow", "Extensions" -> {{"Kernel", "Root" -> ".", "Context" -> {"TestPaclet`"}}, {"FrontEnd"}}]]
