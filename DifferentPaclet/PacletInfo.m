5e118821e1c4da74d001e557270db6a4d2806d78
Brandon Sutton <brandon.sutton@centcorp.tech>
2021-12-28 15:05:27 -0600

    Auto-generated and handled the PacletSite creation

--- DifferentPaclet/PacletInfo.m
+++ DifferentPaclet/PacletInfo.m
@@ -1,12 +1,5 @@
-(* ::Package:: *)
-
-Paclet[
-	"Name" -> "DifferentPaclet",
-	"Version" -> "1.0.2",
-	"Creator" -> "Brandon Sutton <bsutton10294@gmail.com",
-	"Description" -> "Test paclet created for testing Github Action workflow",
-	"Extensions" -> {
-		{"Kernel", "Root"->".", "Context"->{"DifferentPaclet`"}},
-		{"FrontEnd"}
-	}
-]
+Paclet["Name" -> "DifferentPaclet", "Version" -> "1.0.3", 
+ "Creator" -> "Brandon Sutton <bsutton10294@gmail.com", 
+ "Description" -> "Test paclet created for testing Github Action workflow", 
+ "Extensions" -> {{"Kernel", "Root" -> ".", 
+    "Context" -> {"DifferentPaclet`"}}, {"FrontEnd"}}]
--- TestPaclet/PacletInfo.m
+++ TestPaclet/PacletInfo.m
@@ -1,10 +1,5 @@
-Paclet[
-	"Name" -> "TestPaclet",
-	"Version" -> "1.0.2",
-	"Creator" -> "Brandon Sutton <bsutton10294@gmail.com",
-	"Description" -> "Test paclet created for testing Github Action workflow",
-	"Extensions" -> {
-		{"Kernel", "Root"->".", "Context"->{"TestPaclet`"}},
-		{"FrontEnd"}
-	}
-]
+Paclet["Name" -> "TestPaclet", "Version" -> "1.0.3", 
+ "Creator" -> "Brandon Sutton <bsutton10294@gmail.com", 
+ "Description" -> "Test paclet created for testing Github Action workflow", 
+ "Extensions" -> {{"Kernel", "Root" -> ".", "Context" -> {"TestPaclet`"}}, 
+   {"FrontEnd"}}]
