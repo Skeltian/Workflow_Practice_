fc37ed64914df0c79214251958241d5a06f09143
Brandon Sutton <brandon.sutton@centcorp.tech>
2021-12-30 10:53:57 -0600

    Auto-generated and handled the PacletSite creation

--- 02_Information_/PacletSite.m
+++ 02_Information_/PacletSite.m
@@ -1,1 +1,1 @@
-PacletSite[Paclet["Name" -> "02Information", "Version" -> "2.0.0", "Creator" -> "Brandon Sutton <bsutton10294@gmail.com", "Description" -> "Test paclet created for testing Github Action workflow", "Extensions" -> {{"Kernel", "Root" -> ".", "Context" -> {"Information`"}}, {"FrontEnd"}}]]
+PacletSite[Paclet["Name" -> "02Information", "Version" -> "2.0.1", "Creator" -> "Brandon Sutton <bsutton10294@gmail.com", "Description" -> "Test paclet created for testing Github Action workflow", "Extensions" -> {{"Kernel", "Root" -> ".", "Context" -> {"Information`"}}, {"FrontEnd"}}]]
